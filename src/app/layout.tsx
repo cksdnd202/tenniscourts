d671577ec3213a25863e7636862cccc861e44102
강찬웅 <gangchan-ung@gangchan-ung-ui-MacBookPro.local>
2026-01-12 18:35:59 +0900

    add gtm tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,36 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { pretendard } from "./fonts";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className={pretendard.variable}>
-      <body className="font-sans">{children}</body>
+      <head>
+        {/* ✅ GTM - head 영역 */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NSKRJJRG');
+          `}
+        </Script>
+      </head>
+      <body className="font-sans">
+        {/* ✅ GTM - body 바로 뒤 (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NSKRJURG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+
+      </body>
     </html>
   );
 }
